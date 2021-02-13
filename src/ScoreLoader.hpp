0e3b58d9be412b196a7ecfd350a6f6098d925fbb
Sam Vader <samvader13@gmail.com>
2021-02-13 14:28:20 -0600

    Refresh project to reflect .gitignore

--- src/ScoreLoader.hpp
+++ src/ScoreLoader.hpp
@@ -1,24 +1,6 @@
 #pragma once
 #include <string>
-
-
-// a simple struct to hold our min and sec values
-struct Score 
-{
-	int min{ 0 }, sec{ 0 };
-	Score(int m, int s)
-	{
-		min = m;
-		sec = s;
-	}
-	Score(int s)
-	{
-		min = s / 60;
-		s = s % 60;
-		sec = s;
-	}
-	Score() = default;
-};
+#include "Score.hpp"
 
 class ScoreLoader
 {
